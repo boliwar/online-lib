58aef7755a01e5f60e1ad5174f8ec35bbb9b7c35
boliwar <boliwar@gmail.com>
2022-11-22 23:00:08 +0200

    использовать селекторы

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -29,7 +29,7 @@ def main():
     site_url = os.environ['SITE_URL']
     timeout = 10
     start_page = 1
-    end_page = 4
+    end_page = 1
     pattern = re.compile(r'\d+')
     team_books=[]
 
@@ -37,44 +37,38 @@ def main():
         response = requests.get(urljoin(site_url, f"{books_category}/{page}/"))
         response.raise_for_status()
         soup_result = BeautifulSoup(response.text, "html.parser")
-        div_id_content_part = soup_result.body.find('div', attrs={'id': 'content'})
 
-        table_class_d_book_part = div_id_content_part.findAll('table', attrs={'class': 'd_book'})
+        selector = "div#content table.d_book a"
+        a_tags = soup_result.select(selector)
 
-        for part_book in table_class_d_book_part:
-            found_urls = part_book.findAll('a')
+        url = ''
+        for tag in a_tags:
+            if tag.text == 'скачать книгу' or tag.text == 'читатели о книге':
+                url = urljoin(site_url, tag.get('href'))
+                try:
+                    response = requests.get(url)
+                    response.raise_for_status()
+                    check_for_redirect(response)
+                    url_components = urlparse(url)
+                    book_id = pattern.findall(url_components.path)[0]
+                    book = parse_book_page(response, book_id)
+                    payload = {"id": book_id}
+                    file_name = sanitize_filename(book['title'])
+                    download_txt(f'{file_name}.txt', book['url'], payload, books_directory)
+                    download_image(book['img'], None, images_directory)
+                    team_books.append(book)
+                except requests.exceptions.TooManyRedirects:
+                    print(f'TooManyRedirects. ИД = {book_id}, ошибочное перенаправление.')
+                except WrongUrl:
+                    print(f'WrongUrl. ИД = {book_id}, нет ссылки для скачивания.')
+                except requests.exceptions.ConnectionError:
+                    print(f'ConnectionError. Таймаут: {timeout}сек.')
+                    time.sleep(timeout)
+                except requests.exceptions.HTTPError:
+                    print(f'HTTPError. Проверьте урлы: текст = {book["url"]}, картинка = {book["img"]}.')
+                # finally:
+                #     print()
 
-            url = ''
-            for tag in found_urls:
-                if tag.text == 'скачать книгу' or tag.text == 'читатели о книге':
-                    url = urljoin(site_url, tag.get('href'))
-                    break
-            if not url:
-                raise WrongUrl
-
-            try:
-                response = requests.get(url)
-                response.raise_for_status()
-                check_for_redirect(response)
-                url_components = urlparse(url)
-                book_id = pattern.findall(url_components.path)[0]
-                book = parse_book_page(response, book_id)
-                payload = {"id": book_id}
-                file_name = sanitize_filename(book['title'])
-                download_txt(f'{file_name}.txt', book['url'], payload, books_directory)
-                download_image(book['img'], None, images_directory)
-                team_books.append(book)
-            except requests.exceptions.TooManyRedirects:
-                print(f'TooManyRedirects. ИД = {book_id}, ошибочное перенаправление.')
-            except WrongUrl:
-                print(f'WrongUrl. ИД = {book_id}, нет ссылки для скачивания.')
-            except requests.exceptions.ConnectionError:
-                print(f'ConnectionError. Таймаут: {timeout}сек.')
-                time.sleep(timeout)
-            except requests.exceptions.HTTPError:
-                print(f'HTTPError. Проверьте урлы: текст = {book["url"]}, картинка = {book["img"]}.')
-            # finally:
-            #     print()
     pprint(team_books)
     books_json = json.dumps(team_books)
 
