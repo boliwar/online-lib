8d576ec3b3b66fbb07852364f0839f781062f50e
boliwar <boliwar@gmail.com>
2022-11-26 03:49:30 +0200

    Integrate livereload.

--- render_website.py
+++ render_website.py
@@ -6,23 +6,36 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from urllib.parse import urljoin, urlparse
+from livereload import Server
 
-def main():
-    parser = create_parser()
-    command_line_arguments = parser.parse_args()
-    dest_folder = command_line_arguments.dest_folder
+books = []
 
-
+def rebuild():
+    global books
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(books=books,)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    print("Site rebuilt")
+
+
+def main():
+    global books
+    parser = create_parser()
+    command_line_arguments = parser.parse_args()
+    dest_folder = command_line_arguments.dest_folder
+
     load_dotenv()
 
     books_directory = Path(dest_folder, os.environ['BOOKS_DIRECTORY'])
     images_directory = Path(dest_folder, os.environ['IMAGES_DIRECTORY'])
-
-    template = env.get_template('template.html')
 
     filepath = Path(dest_folder, "books.json")
 
@@ -34,14 +47,10 @@ def main():
     for book in books:
         book['img'] = Path(images_directory,os.path.basename(urlparse(book['img']).path))
 
-    rendered_page = template.render(books=books,)
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
-
-    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
-
+    rebuild()
+    server = Server()
+    server.watch('template.html', rebuild)
+    server.serve()
 
 if __name__ == "__main__":
     main()
